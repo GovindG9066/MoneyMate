9d3ba66ed541644314a08df3362e8dbc5cae825c
Govind9066 <gadekargovind575@gmail.com>
2025-02-28 19:38:43 +0530

    adding the js for the button where for calculating the available balance and spend money

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ let error_msg = document.querySelector(".error-msg");
 let exit = document.querySelector(".exit");
 let form_cont = document.querySelector(".form-container");
 let spend = document.querySelector(".add-spend");
+let total_spend=0.00;
+
 
 wallet.addEventListener("keydown", (event) => {
     if (event.key === "Enter") {
@@ -15,7 +17,9 @@ wallet.addEventListener("keydown", (event) => {
             if (amount === null) throw "Please enter a valid input";
             if (amount === 0) throw "Zero in not allowed";
             if (amount < 0) throw "negative number is not allowed";
-            Available_bal.innerHTML = `₹${amount}.00`;
+            current_bal+=amount;
+            Available_bal.innerHTML = `₹${current_bal.toFixed(2)}`;
+            error_msg.innerHTML="";
             wallet.value = "";
         } catch (error) {
             error_msg.innerHTML = error;
@@ -28,16 +32,17 @@ function add_expense() {
     spend.addEventListener("click", () => {
         form_cont.classList.remove("hidden");
         form_cont.classList.add("flex");
-        exit_form();
+
     })
 }
 function exit_form() {
-    exit.addEventListener("click", () => {
-        form_cont.classList.remove("flex");
-        form_cont.classList.add("hidden");
-    })
+    form_cont.classList.remove("flex");
+    form_cont.classList.add("hidden");
 }
 
+if (exit) {
+    exit.addEventListener("click", exit_form);
+}
 
 add_expense();
 
@@ -48,13 +53,30 @@ function history_items() {
         event.preventDefault();
 
         let title = document.querySelector("input[type='text']").value;
-        let amount = document.querySelector("input[type='number']").value;
+        let amount = parseFloat(document.querySelector("input[type='number']").value);
         let category = document.querySelector("select").value;
         let date = document.querySelector("input[type='date']").value;
-
-
         let current_time = new Date();
         let live_Time = current_time.toLocaleTimeString();
+        
+        let money_spend=document.querySelector(".money-spend");
+
+        try {
+            if(current_bal===0)throw "There is no balance in your account"
+            if(isNaN(amount) || amount<= 0) throw "There is no available balance in your account";
+            if(current_bal<amount)throw "Insufficient balance";
+            current_bal-=amount;
+            Available_bal.innerHTML=`₹${current_bal.toFixed(2)}`;
+            total_spend+=amount;
+            money_spend.innerHTML=`₹${total_spend.toFixed(2)}`;
+            
+            
+        } catch (error) {
+            alert(error);
+            exit_form();
+            form_details.reset();
+            return;
+        }
 
 
         let history_item = document.createElement("div");
@@ -64,12 +86,14 @@ function history_items() {
             <p class="text-gray-600">${category}</p>
             <p class="text-gray-500">${date}</p>
             <p class="text-gray-400">${live_Time}</p>
-            <h3 class="text-red-500 font-bold">₹${amount}</h3>
+            <h3 class=" font-bold" style="color: rgb(231 111 81 );">-₹${amount}</h3>
         `;
         history_Cont.appendChild(history_item);
         form_details.reset();
+        exit_form();
     })
 }
+
 
 
 history_items();
