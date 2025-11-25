b2672818d4ad6cef01cfd90247d9ecf6755476e3
Govind9066 <gadekargovind575@gmail.com>
2025-11-26 00:58:14 +0530

    some changes with default pic

--- moneymate_app/models.py
+++ moneymate_app/models.py
@@ -60,15 +60,29 @@ class Expense(models.Model):
     def __str__(self):
         return f"{self.title} - {self.amount}"
 
+# class Profile(models.Model):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE)
+#     profile_image = models.ImageField(upload_to='profile_pics/', default='default.jpg', blank=True, null=True)
+#     dob = models.DateField(null=True, blank=True)
+#     phone = models.CharField(max_length=15, blank=True)
+#     bio = models.TextField(blank=True)
+
+#     def __str__(self):
+#         return self.user.username   Rander.com
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    profile_image = models.ImageField(upload_to='profile_pics/', default='default.jpg', blank=True, null=True)
+    profile_image = models.ImageField(
+        upload_to='profile_pics/',
+        default='images/default.jpg'
+    )
     dob = models.DateField(null=True, blank=True)
     phone = models.CharField(max_length=15, blank=True)
     bio = models.TextField(blank=True)
 
     def __str__(self):
         return self.user.username
+
 
 class Income(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
