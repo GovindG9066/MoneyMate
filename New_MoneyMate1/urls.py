36e4417d0821fed0e13605559ec6b59ccb455931
Govind9066 <gadekargovind575@gmail.com>
2025-11-25 19:36:11 +0530

    some imp changes

--- New_MoneyMate1/urls.py
+++ New_MoneyMate1/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('', lambda request: redirect('landing')),  # home
+    path('', lambda request: redirect('landing')),  # home
     path('', include('moneymate_app.urls')),
 ]
 
--- moneymate_app/urls.py
+++ moneymate_app/urls.py
@@ -2,26 +2,6 @@
 from django.urls import path
 from . import views
 
-# urlpatterns = [
-#     path('', views.landing, name='landing'),   # Default landing page
-#     path('home/', views.home, name='home'),    # After login (protected)
-    
-#     # Auth System
-#     path('login/', views.login_view, name='login'),
-#     path('logout/', views.logout_view, name='logout'),
-
-#     path('register/', views.register_view, name='register'),
-#     path('logout/', views.logout_view, name='logout'),
-    
-
-#     path('add_expense/', views.add_expense, name='add_expense'),
-#     path('add-money/', views.add_money, name='add_money'),
-
-#     path("edit-expense/<int:id>/", views.edit_expense, name="edit_expense"),
-#     path('profile/', views.profile_view, name='profile'),
-
-
-# ]
 
 urlpatterns = [
     path('', views.landing, name='landing'),
